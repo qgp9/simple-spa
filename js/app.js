feddb8be9bb8cfe984be5c1a903f85550505dadf
qgp9 <pauljang9@gmail.com>
2023-10-19 08:42:00 +0300

    move JS to js/app.js

---
+++ js/app.js
@@ -0,0 +1,56 @@
+function PageCounter() {
+  const page = `
+      <div class="container">
+        <div id="navbar">
+          <a href="/index.html">Home</a>
+          <a href="/about.html">About</a>
+        </div>
+        <div class="card">
+          <h1>Count <span id="count-view">0</span></h1>
+          <div class="btn-box">
+            <button class="btn btn-primary" id="btn-increase">Increase</button>
+            <button class="btn btn-danger" id="btn-decrease">Decrease</button>
+          </div>
+        </div>
+      </div>
+    `;
+  document.getElementById("app").innerHTML = page;
+  const countView = document.getElementById("count-view");
+  const btnIncrease = document.getElementById("btn-increase");
+  const btnDecrease = document.getElementById("btn-decrease");
+
+  let count = 0;
+
+  btnIncrease.addEventListener("click", () => {
+    count++;
+    countView.innerText = count;
+  });
+
+  btnDecrease.addEventListener("click", () => {
+    count--;
+    countView.innerText = count;
+  });
+}
+
+function PageAbout() {
+  const page = `
+        <div class="container">
+          <div id="navbar">
+            <a href="/index.html">Home</a>
+            <a href="/about.html">About</a>
+          </div>
+          <div class="card">
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing </p>
+          </div>
+        </div>
+      `;
+  document.getElementById("app").innerHTML = page;
+}
+
+const currentPath = window.location.pathname;
+if (currentPath === "/about.html") {
+  PageAbout();
+} else {
+  PageCounter();
+}
