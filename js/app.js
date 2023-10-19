9f5902d3ac9c4d03d499e7f562bbd586d50dd1a0
qgp9 <pauljang9@gmail.com>
2023-10-19 12:00:40 +0300

    introduce helper for DOM creation; h, div, a,...

--- js/app.js
+++ js/app.js
@@ -1,18 +1,43 @@
+function h(tag, attrs, ...children) {
+  const element = document.createElement(tag);
+  if (attrs instanceof Object && !Array.isArray(attrs)) {
+    for (const attr in attrs) {
+      element.setAttribute(attr, attrs[attr]);
+    }
+  } else {
+    children.unshift(attrs);
+  }
+  children = children.flat();
+  children.forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+    } else {
+      element.appendChild(document.createTextNode(child));
+    }
+  })
+  return element
+}
+
+const div = (...args) => h("div", ...args);
+const h1 = (...args) => h("h1", ...args);
+const p = (...args) => h("p", ...args);
+const a = (...args) => h("a", ...args);
+const button = (...args) => h("button", ...args);
+const span = (...args) => h("span", ...args);
+
 function PageCounter() {
-  const page = `
-      <div class="container">
-        <div id="navbar">
-        </div>
-        <div class="card">
-          <h1>Count <span id="count-view">0</span></h1>
-          <div class="btn-box">
-            <button class="btn btn-primary" id="btn-increase">Increase</button>
-            <button class="btn btn-danger" id="btn-decrease">Decrease</button>
-          </div>
-        </div>
-      </div>
-    `;
-  document.getElementById("app").innerHTML = page;
+  const page = div({class:"container"},
+    div({id:"navbar"}),
+    div({class:"card"},
+      h1("Count", span({id:"count-view"}, 0)),
+      div({class:"btn-box"},
+        button({class:"btn btn-primary", id:"btn-increase"}, "Increase"),
+        button({class:"btn btn-danger", id:"btn-decrease"}, "Decrease")
+      )
+    )
+  );
+  document.getElementById("app").replaceChildren(page);
+
   const countView = document.getElementById("count-view");
   const btnIncrease = document.getElementById("btn-increase");
   const btnDecrease = document.getElementById("btn-decrease");
@@ -31,34 +56,24 @@ function PageCounter() {
 }
 
 function PageAbout() {
-  const page = `
-        <div class="container">
-          <div id="navbar">
-          </div>
-          <div class="card">
-            <h1>About</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing </p>
-          </div>
-        </div>
-      `;
-  document.getElementById("app").innerHTML = page;
+  const page = div({class:"container"},
+    div({id:"navbar"}),
+    div({class:"card"},
+      h1("About"),
+      p("Lorem ipsum dolor sit amet consectetur adipisicing ")
+    )
+  );
+  document.getElementById("app").replaceChildren(page);
 }
 
 function PageContact() {
-  const page = document.createElement("div");
-  page.classList.add("container");
-  const navbar = document.createElement("div");
-  navbar.setAttribute("id", "navbar");
-  page.appendChild(navbar);
-  const card = document.createElement("div");
-  card.classList.add("card");
-  page.appendChild(card);
-  const h1 = document.createElement("h1");
-  h1.innerText = "Contact";
-  card.appendChild(h1);
-  const p = document.createElement("p");
-  p.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing ";
-  card.appendChild(p);
+  const page = div({class:"container"},
+    div({id:"navbar"}),
+    div({class:"card"},
+      h1("Contact"),
+      p("Lorem ipsum dolor sit amet consectetur adipisicing ")
+    )
+  );
   document.getElementById("app").replaceChildren(page);
 }
 
